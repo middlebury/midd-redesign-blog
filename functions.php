829ec59367e2f6496cdd4101ae1f5e4ac0256dff
Zeb Pykosz <zeb.apy@gmail.com>
2016-07-18 14:13:08 -0400

    init commit

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', __( 'Genesis Boilerplate', 'geneplate' ) );
+define( 'CHILD_THEME_URL', 'http://github.com/middlebury/genesis-child-theme-boilerplate/' );
+define( 'CHILD_THEME_VERSION', '0.1.0' );
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Move child theme stylesheet to the end of the line so it takes precedence over plugin stylesheets.
+remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
+add_action( 'wp_enqueue_scripts', 'genesis_enqueue_main_stylesheet', 999 );
+
+//* Add support for structural wraps (max-width has been added to most content-entry elements to allow for full-width images on any page or post. If you don't need full-width images, you can add structural wrap in site-inner.)
+add_theme_support( 'genesis-structural-wraps', array(
+    'header',
+    'nav',
+    // 'subnav',
+    // 'site-inner',
+    'footer-widgets',
+    'footer'
+) );
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var cssnano = require('gulp-cssnano');
+var cmq = require('gulp-combine-mq');
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
+var args = require('yargs').argv;
+var del = require('del');
+
+var PRODUCTION = !!args.production;
+
+var projectName = process.env.PROJECT_NAME || 'midd-redesign-blog';
+
+var plumberErrorHandler = {
+  errorHandler: notify.onError({
+    title: 'Gulp',
+    message: 'Error: <%= error.message %>'
+  })
+};
+
+var paths = {
+  images: {
+    src: './src/images/**/*.{jpg, jpeg, png, svg, gif}',
+    dest: './img'
+  },
+  sass: {
+    src: './src/sass/**/*.scss',
+    dest: './'
+  },
+  js: {
+    src: '',
+    dest: ''
+  }
+}
+
+gulp.task('clean', function() {
+  return del([
+    './style.css',
+    './img/**',
+    './js/**'
+  ]);
+});
+
+gulp.task('images', function() {
+  return gulp.src(paths.images.src)
+    .pipe(imagemin({
+      optimizationLevel: 5
+    }))
+    .pipe(gulp.dest(paths.images.dest));
+});
+
+gulp.task('sass', function() {
+  return gulp.src(paths.sass.src)
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cmq())
+    .pipe(cssnano())
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+    .pipe(gulp.dest(paths.sass.dest))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(paths.js.src)
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+    .pipe(gulpif(PRODUCTION, uglify()))
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+    .pipe(gulp.dest(paths.js.dest));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    open: false,
+    files: [
+      './*.php',
+      paths.sass.src
+    ],
+    proxy: 'localhost/' + projectName
+  });
+});
+
+gulp.task('build', ['clean', 'sass']);
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass.src, ['sass']);
+  gulp.watch(paths.images.src, ['images']);
+  gulp.watch(paths.js.src, ['scripts']);
+});
+
+gulp.task('default', ['sass', 'watch', 'browser-sync']);
